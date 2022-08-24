2faf1d296fef04f7c18158c83f9ea37749a4fca8
alvaro220592 <alvaro220592@gmail.com>
2022-08-24 18:46:39 -0300

    melhorias

--- app.js
+++ app.js
@@ -10,19 +10,19 @@ function processar_retorno() {
     document.getElementById('btn_conciliar').style.display = 'block';
 
     var reader = new FileReader();
-    
+
     reader.readAsText(arquivo);
-    
-    reader.onload = function(e) {
+
+    reader.onload = function (e) {
         var texto_puro = e.target.result;
         var json = JSON.parse(texto_puro);
         mostrar_conteudo_json(json)
-    };    
+    };
 }
 
-function mostrar_conteudo_json(json){
+function mostrar_conteudo_json(json) {
     let tbody = document.getElementById('tbody_conteudo_json');
-    
+
     tbody.innerHTML = '';
 
     json.forEach(item => {
@@ -37,60 +37,76 @@ function mostrar_conteudo_json(json){
     });
 }
 
-function mostrar_boletos(){
+function mostrar_boletos() {
     fetch(`http://localhost:8000/api/boleto/`)
-    .then(res => res.json())
-    .then(data => {
+        .then(res => res.json())
+        .then(data => {
 
-        let tbody = document.getElementById('tbody_boletos')
-        tbody.innerHTML = '';
+            let tbody = document.getElementById('tbody_boletos')
+            tbody.innerHTML = '';
 
-        data.boletos.forEach(item => {            
-            tbody.innerHTML += `
+            data.boletos.forEach(item => {
+                tbody.innerHTML += `
                 <tr>
                 <td>${item.nosso_numero}</td>
                 <td>${item.parcela_id}</td>
                 <td>${item.created_at}</td>           
                 </tr>`
-        });
-    })
-    .catch(err => console.log(err))
+            });
+        })
+        .catch(err => console.log(err))
 }
 
-function mostrar_parcelas(){
+function mostrar_parcelas() {
     fetch(`http://localhost:8000/api/parcela/`)
-    .then(res => res.json())
-    .then(data => {
+        .then(res => res.json())
+        .then(data => {
 
-        let tbody = document.getElementById('tbody_parcelas')
-        tbody.innerHTML = '';
+            let tbody = document.getElementById('tbody_parcelas')
+            tbody.innerHTML = '';
 
-        data.parcelas.forEach(item => {            
-            tbody.innerHTML += `
+            data.parcelas.forEach(item => {
+                tbody.innerHTML += `
                 <tr>
                 <td>${item.id}</td>
                 <td>${item.flg_pago}</td>
                 <td>${item.created_at}</td>           
                 </tr>`
-        });
-    })
-    .catch(err => console.log(err))
+            });
+        })
+        .catch(err => console.log(err))
 }
 
-function conciliar_parcelas(){
+function confirm_conciliar_parcelas() {
+    Swal.fire({
+        text: "Tem certeza?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#649dd3',
+        cancelButtonColor: '#d94545',
+        confirmButtonText: 'Sim',
+        cancelButtonText: 'Não'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            conciliar_parcelas()
+        }
+    })
+}
+
+function conciliar_parcelas() {
     var arquivo = document.getElementById("input").files[0];
 
     document.getElementById("nome_arquivo").innerHTML = arquivo.name;
 
     var reader = new FileReader();
-    
+
     reader.readAsText(arquivo);
-    
-    reader.onload = function(e) {
+
+    reader.onload = function (e) {
         var texto_puro = e.target.result;
         var json = JSON.parse(texto_puro);
-        
-        fetch('http://localhost:8000/api/conciliacao/', {        
+
+        fetch('http://localhost:8000/api/conciliacao/', {
             method: 'POST',
             headers: {
                 // "Content-Type": "Application/Json",
@@ -100,28 +116,28 @@ function conciliar_parcelas(){
                 json: json
             })
         })
-        .then(res => res.json())
-        .then(data => {
-            alerta(data.response)
-            mostrar_parcelas()
-            mostrar_registros()
-        })
-        .catch(e => console.log(e))
-    
-    }; 
+            .then(res => res.json())
+            .then(data => {
+                alerta(data.response)
+                mostrar_parcelas()
+                mostrar_registros()
+            })
+            .catch(e => console.log(e))
+
+    };
 }
 
-function mostrar_registros(){
+function mostrar_registros() {
 
     fetch(`http://localhost:8000/api/registrosWebService/`)
-    .then(res => res.json())
-    .then(data => {
+        .then(res => res.json())
+        .then(data => {
 
-        let tbody = document.getElementById('tbody_registrosWebService')
-        tbody.innerHTML = '';
+            let tbody = document.getElementById('tbody_registrosWebService')
+            tbody.innerHTML = '';
 
-        data.registros_webservice.forEach(item => {
-            tbody.innerHTML += `
+            data.registros_webservice.forEach(item => {
+                tbody.innerHTML += `
                 <tr>
                     <td>${item.nosso_numero}</td>
                     <td>${item.flg_pago}</td>
@@ -129,15 +145,39 @@ function mostrar_registros(){
                     <td>${item.obs}</td>
                     <td>${item.data_registro}</td>                
                 </tr>`
-        });
-    })
-    .catch(err => console.log(err))
+            });
+        })
+        .catch(err => console.log(err))
 }
 
-function alerta(mensagem){
-    Swal.fire({
-        text: mensagem,
+function alerta(mensagem) {
+    // Swal.fire({
+    //     icon: 'success',
+    //     text: mensagem,
+    //     showConfirmButton: false,
+    //     timer: 2000
+    // })
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
         showConfirmButton: false,
-        timer: 2000
-    })
+        timer: 3000,
+        timerProgressBar: true,
+        backdrop: `
+    rgba(0,0,123,0.4)
+    fullscreen
+    no-repeat
+  `
+,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: mensagem
+      })
 }
